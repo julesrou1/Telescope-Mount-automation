3ca4c10e41a18abe5b8a2d6bf58a26d4345e991b
julesrou1 <julesrou123@gmail.com>
2020-10-21 13:24:12 +0200

    2nd motor
    Board change, defines pin for 2nd motor

--- Basic motor control/src/main.cpp
+++ Basic motor control/src/main.cpp
@@ -1,54 +1,77 @@
 #include <Arduino.h>
-
-// defines pins numbers
+// defines pins numbers FOR MOTOR 1
 // Step pin and directional pin
-const int stepPin = 3; 
-const int dirPin = 4; 
+const int M1stepPin = 29; 
+const int M1dirPin = 31; 
 //Control Microstepping Resolution
-const int ms1pin = 8;
-const int ms2pin = 9;
-const int ms3pin = 10;
-//Use for switch
-int inPin = 2;
-int outPin=12;
+const int M1ms1pin = 23;
+const int M1ms2pin = 25;
+const int M1ms3pin = 27;
 
+// defines pins numbers FOR MOTOR 2
+// Step pin and directional pin
+const int M2stepPin = 43; 
+const int M2dirPin = 45; 
+//Control Microstepping Resolution
+const int M2ms1pin = 37;
+const int M2ms2pin = 39;
+const int M2ms3pin = 41;
+
+//Use for switch
+int inPin = 48;
+int outPin=53;
 void setup() {
 // Sets the two pins as Outputs
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
-  pinMode(ms1pin,OUTPUT);
-  pinMode(ms2pin,OUTPUT);
-  pinMode(ms3pin,OUTPUT);
+  pinMode(M1stepPin,OUTPUT); 
+  pinMode(M1dirPin,OUTPUT);
+  pinMode(M1ms1pin,OUTPUT);
+  pinMode(M1ms2pin,OUTPUT);
+  pinMode(M1ms3pin,OUTPUT);
+
+  pinMode(M2stepPin,OUTPUT); 
+  pinMode(M2dirPin,OUTPUT);
+  pinMode(M2ms1pin,OUTPUT);
+  pinMode(M2ms2pin,OUTPUT);
+  pinMode(M2ms3pin,OUTPUT);
 // Control for switch
   pinMode(inPin, INPUT);
   pinMode(outPin, OUTPUT);
   digitalWrite(outPin,HIGH);
 //Setup direction and microstep resolution to 1/16
-  digitalWrite(dirPin,HIGH);
-  digitalWrite(ms1pin,LOW);
-  digitalWrite(ms2pin,LOW);
-  digitalWrite(ms3pin,LOW);
+  digitalWrite(M1dirPin,HIGH);
+  digitalWrite(M1ms1pin,HIGH);
+  digitalWrite(M1ms2pin,HIGH);
+  digitalWrite(M1ms3pin,HIGH);
+  digitalWrite(M2dirPin,HIGH);
+  digitalWrite(M2ms1pin,LOW);
+  digitalWrite(M2ms2pin,LOW);
+  digitalWrite(M2ms3pin,LOW);
   pinMode(13, OUTPUT);
 }
 
 void setAngularMotion(float angle){
     int step=round(angle/(1.8));
     for(int x = 0; x < step; x++) {
-      digitalWrite(stepPin,HIGH); 
-      delayMicroseconds(500); 
-      digitalWrite(stepPin,LOW); 
-      delayMicroseconds(500); 
+      digitalWrite(M1stepPin,HIGH); 
+      digitalWrite(M2stepPin,HIGH); 
+      delayMicroseconds(1000);
+      digitalWrite(M1stepPin,LOW);
+      digitalWrite(M2stepPin,LOW);
+      delayMicroseconds(1000);  
   }
 }
 
+
 void loop() {
-  digitalWrite(dirPin,HIGH);
+  digitalWrite(M1dirPin,HIGH);
   if(digitalRead(inPin) == HIGH){
-     for(int x = 0; x < 200; x++) {
-      digitalWrite(stepPin,HIGH); 
-      delayMicroseconds(500); 
-      digitalWrite(stepPin,LOW); 
-      delayMicroseconds(500); 
+     for(int x = 0; x < 3200; x++) {
+      digitalWrite(M1stepPin,HIGH); 
+      digitalWrite(M2stepPin,HIGH); 
+      delayMicroseconds(1000); 
+      digitalWrite(M1stepPin,LOW);
+      digitalWrite(M2stepPin,LOW);
+      delayMicroseconds(1000); 
   }
   }
     else{
