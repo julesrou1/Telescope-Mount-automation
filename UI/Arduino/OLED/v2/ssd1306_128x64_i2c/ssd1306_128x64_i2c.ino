dfaeb8ea98d07ab09efc5040b008d5caa643c3e5
Jairkhar <germain.pierrejean@gmail.com>
2021-03-23 16:35:51 +0100

    Create ssd1306_128x64_i2c.ino

---
+++ UI/Arduino/OLED/v2/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
@@ -0,0 +1,71 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32// OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void display1(int n){
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  if(n==1){display.println("Hello, wtf!");}
+  if(n==2){display.println("Hello, Jules!");}
+  display.display(); 
+  }
+
+
+
+void display2(double ra, double dec){
+  display.clearDisplay();
+
+  //affichage ra
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,8);
+  display.println("ra:");
+  display.setTextSize(1);
+  display.setCursor(20,8);
+  display.println(ra);
+
+  //affichage dec
+  display.setTextSize(1);
+  display.setCursor(0,18);
+  display.println("dec:");
+  display.setTextSize(1);
+  display.setCursor(25,18);
+  display.println(dec);
+
+
+  display.display();
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+}
+
+void loop() {
+//  display1(1);
+//  delay(1000);
+//  display1(2);
+  Serial.end();
+   delay(1000);
+   display.clearDisplay();
+   display2(45.58976,-0.12453);
+  
+}
